dd52a882d56f0cc392e0a6d297cf4a1eb79e83ab
PiotrCzap <piotrczapmlawa@interia.pl>
2026-08-28 18:59:57 +0200

    Added Engine_draw_text_better

--- src/main.c
+++ src/main.c
@@ -8,8 +8,9 @@ char ENGINE_VERSION[16] = "Version: 0.0.1";
 int window_size_x = 800;
 int window_size_y = 600;
 float FPS = 60.0f;
-Color GRAY_COLOR = { 43, 43, 43, 255 }; // R, G, B, Alpha (przezroczystość)
-Color DARK_GRAY_COLOR = { 30, 30, 30, 255 }; // R, G, B, Alpha (przezroczystość
+Color GRAY_COLOR = { 40, 40, 40, 255 };
+Color DARK_GRAY_COLOR = { 30, 30, 30, 255 };
+Color LIGHT_GRAY_COLOR = { 70, 70, 70, 255 };
 
 Font font;
 
@@ -17,6 +18,7 @@ struct GameObject player;
 struct GameObject test_text;
 
 static inline void Engine_draw_rectangle_shape(const float pos_x, const float pos_y, const float size_x, const float size_y, const Color color);
+static inline void Engine_draw_text_better(const Font font, const char text[], const Vector2 crap_engine_text_position, const Vector2 crap_engine_text_origin, const float rotation, const float font_size, const float spacing, const Color color);
 
 // =================================================================================================
 // Funkcje z GUI
@@ -26,14 +28,9 @@ static inline void Project_window()
 {
     Engine_draw_rectangle_shape(0, 0, 800, 600, GRAY_COLOR);
     Engine_draw_rectangle_shape(0, 0, 256, 600, DARK_GRAY_COLOR);
-
-    Vector2 crap_engine_text_position = { 10.0f, 10.0f };
-    Vector2 crap_engine_text_origin = { 0.0f, 0.0f };
-    DrawTextPro(font, ENGINE_NAME, crap_engine_text_position, crap_engine_text_origin, 0.0f, 32.0f, 2.0f, WHITE);
-
-    Vector2 crap_engine_version_text_position = { 10.0f, 580.0f };
-    Vector2 crap_engine_version_text_origin = { 0.0f, 0.0f };
-    DrawTextPro(font, ENGINE_VERSION, crap_engine_version_text_position, crap_engine_version_text_origin, 0.0f, 16.0f, 2.0f, WHITE);
+    Engine_draw_text_better(font, ENGINE_NAME, (Vector2){10.0f, 10.0f}, (Vector2){0.0f, 0.0f}, 0.0f, 32.0f, 2.0f, WHITE);
+    Engine_draw_text_better(font, ENGINE_VERSION, (Vector2){10.0f, 580.0f}, (Vector2){0.0f, 0.0f}, 0.0f, 16.0f, 2.0f, WHITE);
+    
 }
 
 // =================================================================================================
@@ -52,6 +49,17 @@ static inline void Engine_texture_unloader()
     UnloadTexture(null_txt);
 }
 
+// FUNKCJA RYSUJĄCA TEKSTURE
+/**
+ * @brief PARAMETRY FUNKCJI "Engine_draw_rectangle_shape_with_texture"
+ * @param texture tekstura
+ * @param pos_x pozycja tekstury w osi x
+ * @param pos_y pozycja tekstury w osi y
+ * @param size_x wielkość tekstury w osi x
+ * @param size_y wielkość tekstury w osi y
+ * @param rotation obrót tekstury
+ * @param color kolor tekstury
+ */
 static inline void Engine_draw_rectangle_shape_with_texture(const Texture2D texture, const float pos_x, const float pos_y, const float size_x, const float size_y, float rotation, const Color color)
 {
     Rectangle sourceRec = { 0.0f, 0.0f, (float)texture.width, (float)texture.height };
@@ -65,6 +73,10 @@ static inline void Engine_draw_rectangle_shape_with_texture(const Texture2D text
 // Standardowe Funkcje
 // =================================================================================================
 
+static inline void Engine_draw_text_better(const Font font, const char text[],const Vector2 crap_engine_text_position, const Vector2 crap_engine_text_origin, const float rotation, const float font_size, const float spacing, const Color color)
+{
+    DrawTextPro(font, text, crap_engine_text_position, crap_engine_text_origin, rotation, font_size, spacing, color);
+}
 
 // FUNKCJA RYSUJĄCA KWADRAT/PROSTOKĄT
 /**
@@ -140,6 +152,7 @@ int main(void)
     InitWindow(window_size_x, window_size_y, ENGINE_NAME);
     SetTargetFPS(FPS);
     Engine_texture_loader();
+    
 
 while (!WindowShouldClose())
 {
@@ -158,6 +171,7 @@ while (!WindowShouldClose())
 
         BeginDrawing();
             ClearBackground(BLACK);
+            
             Project_window();
 
         EndDrawing();
